b5c32d5b96293f2f67c9f5502ca1333523557c28
remigoldfarb <112513996+remigoldfarb@users.noreply.github.com>
2022-11-08 15:23:10 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,22 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video.autoplay = false;
+	video.loop = false;
+	console.log("Auto Play is set to" +video.autoplay) 
+	console.log("Loop is set to" +video.loop) 
+
+});
+
+ document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	document.querySelector("#volume").innerHTML= video.volume *100 + "%"
+	video.play()
+ });
+ 
+ document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+   video.pause()
+});
+
